c4ee55b0a86e0121c365096ce2646e39df500b55
andriyprodan <prodandgri@gmail.com>
2024-10-22 09:37:56 +0300

    fix url bug

--- bot.py
+++ bot.py
@@ -12,32 +12,32 @@ bot = telebot.TeleBot(TOKEN)
 # Data for categories and corresponding Google Drive links
 DATA = {
     "Освіта": {
-        "Олевський ліцей № 1": "TODO",
-        "Олевський ліцей № 2": "TODO",
-        "Олевський ліцей № 3": "TODO",
-        "ЦДЮТ": "TODO",
+        "Олевський ліцей № 1": "https://drive.google.com/drive/folders/TODO",
+        "Олевський ліцей № 2": "https://drive.google.com/drive/folders/TODO",
+        "Олевський ліцей № 3": "https://drive.google.com/drive/folders/TODO",
+        "ЦДЮТ": "https://drive.google.com/drive/folders/TODO",
         "Мистецька школа “Олевська музична школа”": "https://drive.google.com/drive/folders/19eb63hU827yzmbPbKEs8vCSMK0dbey5K",
-        'КУ "Олевський міжшкільний ресурсний центр" Олевської міської ради': "TODO",
-        "ДНЗ «Олевський професійний ліцей»": "TODO",
+        'КУ "Олевський міжшкільний ресурсний центр" Олевської міської ради': "https://drive.google.com/drive/folders/TODO",
+        "ДНЗ «Олевський професійний ліцей»": "https://drive.google.com/drive/folders/TODO",
     },
     "Медицина": {
         'КНП "Олевський Центр ПМД" Олевської міської ради': "https://drive.google.com/drive/folders/11Wy9jUvHF7Qgx7HRd1EcAeK0GGvu0AUg",
-        'Комунальна установа "Олевська центральна районна лікарня"': "TODO",
-        'Медичний центр “IVA”': "TODO",
-        'Медичний центр “Наш лікар”': "TODO",
+        'Комунальна установа "Олевська центральна районна лікарня"': "https://drive.google.com/drive/folders/TODO",
+        'Медичний центр “IVA”': "https://drive.google.com/drive/folders/TODO",
+        'Медичний центр “Наш лікар”': "https://drive.google.com/drive/folders/TODO",
     },
     "Розваги": {
-        "Міський Будинок культури": "TODO",
-        "Олевська публічна біблотека": "TODO",
-        "Кафе та ресторани міста": "TODO",
+        "Міський Будинок культури": "https://drive.google.com/drive/folders/TODO",
+        "Олевська публічна біблотека": "https://drive.google.com/drive/folders/TODO",
+        "Кафе та ресторани міста": "https://drive.google.com/drive/folders/TODO",
     },
     "Послуги": {
-        "Центр надання адміністративних послуг": "TODO",
-        "Олевський РЕМ": "TODO",
-        "ПриватБанк": "TODO",
+        "Центр надання адміністративних послуг": "https://drive.google.com/drive/folders/TODO",
+        "Олевський РЕМ": "https://drive.google.com/drive/folders/TODO",
+        "ПриватБанк": "https://drive.google.com/drive/folders/TODO",
         "ОщадБанк": "https://drive.google.com/drive/folders/1-C_PuvZ-_fV_TSQ4CSyRYOLffqyp5L8I",
         "Відділ обслуговування громадян № 4 (пенсійний фонд)": "https://drive.google.com/drive/folders/1-C_PuvZ-_fV_TSQ4CSyRYOLffqyp5L8I",
-        "Олевське управління Коростенської філії Житомирського ОЦЗ": "TODO",
+        "Олевське управління Коростенської філії Житомирського ОЦЗ": "https://drive.google.com/drive/folders/TODO",
     },
 }
 
